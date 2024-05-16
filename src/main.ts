5564ee30c7b7daac367ab7e3a5e4c175aa4825c4
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2024-05-16 00:04:03 -0300

    feat: criando service prisma

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        
+        // Retira os logs do terminal no momento de execução
+        // logger: false
+    });
+
     await app.listen(3333);
 }
 
