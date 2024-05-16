844e232d127dab6713c17436ddb06b6fc732e288
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2024-05-16 18:33:09 -0300

    feat: criando decorator autentocacao

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Post, UseGuards } from "@nestjs/common";
 
+import { UserPayload } from "src/auth/jwt.strategy";
 import { JwtAutGuard } from "src/auth/jwt-auth.guard";
 import { PrismaService } from "src/prisma/prisma.service";
+import { CurrentUser } from "src/auth/current-user-decorator";
 
 @Controller("/questions")
 @UseGuards(JwtAutGuard)
@@ -9,7 +11,8 @@ export class CreateQuestionsController {
     constructor(private prisma: PrismaService) {}
 
     @Post()
-    async handle() {
+    async handle(@CurrentUser() user: UserPayload) {
+        console.log(user.sub);
         return "Ok";
     }
 }
