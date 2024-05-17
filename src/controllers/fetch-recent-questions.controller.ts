1c692d408f49d3bc2336149c2155217ffe161d18
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2024-05-16 22:25:30 -0300

    feat: banco de dados isolado nos testes

--- src/controllers/fetch-recent-questions.controller.ts
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -24,7 +24,7 @@ export class FetchRecentQuestionsController {
     async handle(
         @Query("page", queryValidationPipe) page: PageQueryParamSchema,
     ) {
-        const perPage = 1;
+        const perPage = 2;
 
         const questions = await this.prisma.questions.findMany({
             take: perPage,
--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -7,6 +7,9 @@ export default defineConfig({
         include: ["**/*.e2e-spec.ts"],
         globals: true,
         root: "./",
+        setupFiles: [
+           "./test/setup-e2e.ts" 
+        ]
     },
     plugins: [
         tsConfigPaths(),
